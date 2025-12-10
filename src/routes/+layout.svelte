2be0fcbe94a097dc86ef6d9a3a260d83a0a38627
DerBaumann <natur5210@gmail.com>
2025-12-10 09:24:00 +0100

    Remove the now redundant navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,15 +9,6 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="navbar bg-base-100 shadow-sm">
-  <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">CloudChan</a>
-  </div>
-  <div class="flex-none">
-    <a href="/add">Neuer Post</a>
-  </div>
-</div>
-
 <main class="py-16 px-8">
   {@render children()}
 </main>
