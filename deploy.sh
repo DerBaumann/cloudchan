9b1589b5d0e0b41bf8c4fa071219aa717d52c4fa
DerBaumann <natur5210@gmail.com>
2026-01-03 13:23:12 +0100

    Remove sudo check from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@
 # This is a deployment script for a aws ec2 instance running amazon linux.
 # If you want to deploy this on something else, you might have to adjust this file.
 
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root"
-  exit 1
-fi
-
 # Install Node & NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
